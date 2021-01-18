cbd7c91f4982fb42b74c89242a04425a889d5c03
Nerdpraise <praiseajayi2@gmail.com>
2021-01-18 17:03:20 +0100

    Added spinner for the signup

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,5 +1,4 @@
-// const BASE_URL = 'https://bunny-todo.herokuapp.com/v1/'
-const BASE_URL = 'http://localhost:8000/v1/'
+const BASE_URL = 'https://bunny-todo.herokuapp.com/v1/'
 
 
 export default function getUserToken() {
